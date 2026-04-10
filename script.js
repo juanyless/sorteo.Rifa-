1dfcaceee035d18ea71dbddbfa485164b72a07e5
juankalescano@outlook.es <tu@email.com>
2026-04-10 13:09:39 -0300

    Solución error de link en el alias y mejora de mensaje WhatsApp

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ function finalizarCompraMultiple() {
     
     if (confirmar) {
         // Redirección a WhatsApp con todos los números
-        const mensaje = `¡Hola! Quiero los números: ${listaNumeros}. Total a pagar: $${total}. Envia el comrovante con tu nombre, gracias por tu compra!!! Mucha suerte!!!! ${alias}`;
+        const mensaje = `¡Hola! Quiero los números: ${listaNumeros}. Total a pagar: $${total}. Envia el comprovante con tu nombre, gracias por tu compra!!! Mucha suerte!!!! ${alias}`;
         window.open(`https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`, '_blank');
 
         // Guardar todos en Firebase
